97e941dc080719f6c1456ec09d71c16ea8ad3192
rbadour <ryanbadour@rocketinnovationstudio.ca>
2021-09-14 10:36:40 -0400

    Update test

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 function resize() {
 	var outer = document.querySelector(".outer");
-  outer.style.height = window.innerHeight + "px";
+  //outer.style.height = window.innerHeight + "px";
   
   var rect = document.querySelector(".footer").getBoundingClientRect();
   document.querySelector(".log").innerText = outer.style.height + " -- " + rect.top;
